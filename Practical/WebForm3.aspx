0b79f5490d03dad6d1f983b9ec44de33c4f09922
TARUMT <TARUMT@D203G-PC07>
2023-12-01 09:29:56 +0800

    sdfs

--- Practical/WebForm3.aspx
+++ Practical/WebForm3.aspx
@@ -9,7 +9,20 @@
 <body>
     <form id="form1" runat="server">
         <div>
-        </div>
+            This is to confirm that you have ordered:<br />
+            <br />
+            <asp:Label ID="lblOrder" runat="server"></asp:Label>
+            <br />
+            <br />
+            The time now is
+            <asp:Label ID="lblTimeNow" runat="server"></asp:Label>
+            .<br />
+            You will receive your order within 25 minutes.<br />
+            We will reach at your doorstep before or at
+            <asp:Label ID="lblReceiveTime" runat="server"></asp:Label>
+            .<br />
+            <br />
+            Thank you.</div>
     </form>
 </body>
 </html>
--- Practical/WebForm3.aspx.cs
+++ Practical/WebForm3.aspx.cs
@@ -11,7 +11,28 @@ namespace Practical
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(PreviousPage != null && PreviousPage.IsCrossPagePostBack)
+            {
+                if (PreviousPage.FindControl("cbSet1").Checked)
+                {
+                    lblOrder.Text = PreviousPage.FindControl("txtQ1").Text + " - Menu Set 1</br>";
+                }
+                if (PreviousPage.FindControl("cbSet2").Checked)
+                {
+                    lblOrder.Text = PreviousPage.FindControl("txtQ2").Text + " - Menu Set 2</br>";
+                }
+                if (PreviousPage.FindControl("cbSet3").Checked)
+                {
+                    lblOrder.Text = PreviousPage.FindControl("txtQ3").Text + " - Menu Set 3</br>";
+                }
 
+                lblOrder.Text = "Total Price = " +
+                    PreviousPage.FindControl("lblTotalPrice").ToString("C")
+                    + "</br>";
+            }
+
+            lblTimeNow.Text = DateTime.Now.ToShortTimeString();
+            lblReceiveTime.Text = DateTime.Now.AddMinutes(25).ToShortTimeString();
         }
     }
 }
